2297bb3d3bf5cb1cc102f74da2b3dabcaf4d6822
Claire Wright <claire.wright.mi@gmail.com>
2023-10-03 13:32:54 -0700

    Hybrid alignment with homography and perspective transform

--- img_alignment.py
+++ img_alignment.py
@@ -215,14 +215,22 @@ def alignImgs(dlg, source_img_path, table):
         source_pts.append(source_pt)
         dest_pts.append(dest_pt)
         x+=1
-    print(x)
 
     source_pts_array = np.float32(source_pts)
     dest_pts_array = np.float32(dest_pts)
 
     # align image using perspective transform
-    matrix, mask = cv2.findHomography(source_pts_array, dest_pts_array, cv2.RANSAC,5.0)
+    homography, mask = cv2.findHomography(source_pts_array, dest_pts_array, cv2.RANSAC,5.0)
+    mask = mask.flatten()
+
+    index = np.nonzero(mask)
+    
+    source_pts_good = source_pts_array[index]
+    print(source_pts_good)
+    dest_pts_good = dest_pts_array[index]
+
     h,w = img.shape[:2]
+    matrix = cv2.getPerspectiveTransform(source_pts_good, dest_pts_good)
     aligned_img = cv2.warpPerspective(img, matrix, (w, h))
 
     # save aligned image to temporary file
