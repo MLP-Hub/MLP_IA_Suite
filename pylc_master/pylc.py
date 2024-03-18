60556010e7db4c4195e78ff2d2ca2b1e29d4e6d6
Claire Wright <claire.wright.mi@gmail.com>
2024-03-18 09:18:57 -0700

    Removed default directory creation

--- pylc_master/pylc.py
+++ pylc_master/pylc.py
@@ -16,23 +16,12 @@ import sys
 
 # Import the code for pylc
 from mlp_ia_suite.pylc_master.test import tester
-from mlp_ia_suite.pylc_master.config import defaults
 
 
 def main(args):
     """
     Main application handler
     """
-    # Get parsed input arguments
-
-    #GET INPUT ARGUMENTS FROM QGIS -> make sure to set defaults
-
-    # ensure data directories exist in project root
-    # HOW MANY OF THESE DO I NEED?
-    dirs = [defaults.root, defaults.db_dir, defaults.save_dir, defaults.model_dir, defaults.output_dir]
-    for dir_path in dirs:
-        if not os.path.exists(dir_path):
-            os.mkdir(dir_path)
 
     # execute processing function
     tester(args)
